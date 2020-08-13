a8a8893a81089e17d53d80045e41413dac5bbdf7
alex-gdv <alejandrogdelv@gmail.com>
2020-08-13 16:59:10 +0100

    Update exponential_smoothing.py

--- Models/exponential_smoothing.py
+++ Models/exponential_smoothing.py
@@ -3,39 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
 
-bitcoin_data = pd.read_csv('Data/bitcoin_data.csv')
-bitcoin_data = bitcoin_data['Close']
-
-print(bitcoin_data)
-train_data = bitcoin_data[:2500]
-test_data = bitcoin_data[2500:]
-
-print(test_data)
-
-
-fit1 = SimpleExpSmoothing(train_data).fit()
-fcast1 = fit1.predict(start=2490, end=2500).rename()
-print(fcast1)
-
-fit2 = Holt(train_data, damped=True).fit()
-fcast2 = fit2.predict(start=2490, end=2500).rename()
-fit3 = Holt(train_data, exponential=True).fit()
-fcast3 = fit3.predict(start=2490, end=2500).rename()
-fit4 = Holt(train_data, damped=True, exponential=True).fit()
-fcast4 = fit4.predict(start=2490, end=2500).rename()
-
-
-train_data[2490:].plot()
-test_data.head(n=1).plot(marker='o', label="test value", color="purple", legend=True)
-fcast1.plot(legend=True, marker='x', label='alpha={}'.format(fit1.model.params['smoothing_level']))
-fcast2.plot(legend=True, marker='x', label="Additive damped trend")
-fcast3.plot(legend=True, marker='x', label="Additive exponential trend")
-fcast4.plot(legend=True, marker='x', label="Additive exponential and damped trend")
-# fit1.fittedvalues.plot(ax=ax, color='blue')
-
-plt.show()
-
-# data = [446.6565,  454.4733,  455.663 ,  423.6322,  456.2713,  440.5881, 425.3325,  485.1494,  506.0482,  526.792 ,  514.2689,  494.211 ]
-# index= pd.date_range(start='1996', end='2008', freq='A')
-# oildata = pd.Series(data, index)
-# print(oildata)
+def exponential_smoothing():
+    bitcoin_data = pd.read_csv('C:\\Users\\alexg\\Desktop\\TCF\\Data\\bitcoin_data.csv')
+    bitcoin_data = bitcoin_data['Close']
+    fit4 = Holt(bitcoin_data, damped=True, exponential=True).fit()
+    fcast4 = fit4.predict(start=len(bitcoin_data), end=len(bitcoin_data)).rename()
+    # type of variable prediction is numpy.float64
+    prediction = fcast4[len(bitcoin_data)]
