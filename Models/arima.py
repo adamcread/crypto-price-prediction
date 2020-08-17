fa267b67d23bfa0c0fe6ccc825659dbd86c1a67a
adamcread <62510224+adamcread@users.noreply.github.com>
2020-08-17 01:34:48 +0100

    arima model v1.0
    - fully functioning arima model
    - reads from historical gbq and develops a portfolio based on the model prediction

--- Models/arima.py
+++ Models/arima.py
@@ -1,14 +1,82 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from statsmodels.tsa.arima_model import ARIMA
+from pypfopt import risk_models
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt.discrete_allocation import DiscreteAllocation
+from datetime import date
 
-def arima():
-    # change later
-    bitcoin_data = pd.read_csv('C:\\Users\\alexg\\Desktop\\TCF\\Data\\bitcoin_data.csv')
-    bitcoin_data = bitcoin_data.drop(labels=['Date', 'Open', 'High', 'Low', 'Volume', 'Market Cap'], axis=1)
-    history = bitcoin_data.to_numpy()
-    model = ARIMA(history, order=(5,1,0))
-    model_fit = model.fit(disp=0)
-    output = model_fit.forecast()
-    yhat = output[0]
+def arima(event=None, context=None):
+    # get historical crypto data from gbq (sort by date to ensure data is in correct order)
+    historical_data_query = """
+        SELECT *
+        FROM `crypto-prediction-286314.crypto_predictor.historical_data`
+    """
+    historical_data = pd.read_gbq(historical_data_query).sort_values(by='date').reset_index(drop=True)
+
+    # initialise variables to store expected returns and predicted values
+    expected_returns = pd.Series(dtype='float64')
+    predicted = pd.DataFrame()
+
+    # list currencies to trade with
+    currencies = ["bitcoin", "ethereum", "xrp", "litecoin", "dash"]
+    for currency in currencies:
+        # train model on historical data
+        crypto_data = historical_data['{}Close'.format(currency)].to_numpy()
+        model = ARIMA(crypto_data, order=(5,1,0))
+        model_fit = model.fit(disp=0)
+
+        # make prediction about next day closing price
+        output = model_fit.forecast()
+
+        predicted_value = output[0]
+        closing_value = crypto_data[-1]
+
+        # store predicted value and expected return (predicted percentage increase)
+        predicted['{}Predicted'.format(currency)] = predicted_value
+        expected_returns['{}Close'.format(currency)] = (predicted_value-closing_value) / closing_value
+
+    # get portfolio data from gbq (sort by date to ensure data is in correct order)
+    pf_query = """
+            SELECT *
+            FROM `crypto-prediction-286314.crypto_predictor.arima_portfolio`
+    """
+    pf_data = pd.read_gbq(pf_query).sort_values(by='date').reset_index(drop=True)
+
+    # obtain most recent closing prices and portfolio
+    latest_prices = historical_data.drop('date', axis=1).iloc[-1]
+    pf_latest = pf_data.iloc[-1]
+
+    # calculate updated portfolio value
+    pf_value = sum([pf_latest[c+'Bought']*latest_prices[c+'Close'] for c in currencies]) + float(pf_latest['pfUnallocated'])
+
+    # use efficient frontier to obtain weightings of coins to purchase
+    # drop date as it does not need to be included in covariance matrix
+    S = risk_models.risk_matrix(historical_data.drop('date', axis=1))
+    ef = EfficientFrontier(expected_returns, S, gamma=5)
+    ef.min_volatility()
+    cleaned_weights = ef.clean_weights()
+
+    # use weightings to calculate how many coins to purchase
+    da = DiscreteAllocation(cleaned_weights, latest_prices, total_portfolio_value=pf_value)
+    allocation, unallocated = da.lp_portfolio()
+
+
+    # for each coin store tomorrows's predicted, today's close, coins bought
+    # also store current portfolio value + unallocated money (to help calculate updated portfolio value)
+    pf_updated = {}
+    for currency in currencies:
+        pf_updated['{}Close'.format(currency)] = float(latest_prices['{}Close'.format(currency)])
+        pf_updated['{}Predicted'.format(currency)] = float(predicted['{}Predicted'.format(currency)])
+        pf_updated['{}Bought'.format(currency)] = float(allocation.get('{}Close'.format(currency), 0))
+    pf_updated['pfValue'] = pf_value
+    pf_updated['pfUnallocated'] = unallocated
+
+    # put new portfolio into dataframe and insert today's date
+    pf_updated = pd.DataFrame(pf_updated, index=[0])
+    pf_updated.insert(0, 'date', date.today().strftime("%Y-%m-%d"))
+
+    # add new portfolio to existing portfolio data and push to gbq
+    pf_data = pd.concat([pf_data, pf_updated], ignore_index=True)
+    pf_data.to_gbq(destination_table='crypto_predictor.arima_portfolio',
+                project_id="crypto-prediction-286314",
+                if_exists='replace')
